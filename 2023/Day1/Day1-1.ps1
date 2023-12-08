d7feb2d733572719730f5d649927d8a6e94bf3bf
ReechK <34216873+ReechK@users.noreply.github.com>
2023-12-08 16:25:52 -0500

    getting there

--- 2023/Day1/Day1-1.ps1
+++ 2023/Day1/Day1-1.ps1
@@ -1,1 +1,84 @@
-##day 1 
+
+
+
+$tot = 0
+
+foreach ($line in Get-Content '.\AoC\Day1\Day1.0.txt') {
+    #get ready for a line
+    $firstnum = -1
+    $lastnum = -1
+    
+    $len = $line.Length
+    ##write-host ".$line. is $len long"
+    $curpos = 0
+    while ( $curpos -lt $len) {
+        
+        #do stuff
+        $str = $line[$curpos..$len] -join ''
+        $char = $line[$curpos..$curpos] -join ''
+        ## Write-Host ".$str."
+        ## write-host ".$char."
+        $int = -1
+
+        switch -regex ($str) {
+            "^one" {
+                $int = 1; break 
+            }
+            "^two" {
+                $int = 2; break 
+            }
+            "^three" {
+                $int = 3; break 
+            }
+            "^four" {
+                $int = 4; break 
+            }
+            "^five" {
+                $int = 5; break 
+            }
+            "^six" {
+                $int = 6; break 
+            }
+            "^seven" {
+                $int = 7; break 
+            }
+            "^eight" {
+                $int = 8; break 
+            }
+            "^nine" {
+                $int = 9; break 
+            }
+            "^zero" {
+                $int = 0; break 
+            }
+        }
+ 
+        if ( $int -eq -1) {
+            if ( $char -match "\d" ) {
+                $int = [int]$char
+            }
+        }
+
+        if ( $int -ne -1) {
+            if ($firstnum -eq -1) {
+                $firstnum = $int
+            }
+            else {
+                $lastnum = $int
+            }
+        }        
+        $curpos += 1
+    }
+
+
+    if ( $lastnum -eq -1 ) {
+        $lastnum = $firstnum
+    }
+    $num = ( $firstnum * 10 ) + $lastnum
+    $tot += $num 
+    Write-Host "$line -> ($firstnum $lastnum) $num -> $tot"
+    ##write-host "firstnum $firstnum lastnum $lastnum which is $num making the total $tot"
+    ##Write-Host "========================================================="
+    #finish line
+}
+Write-Host "the total is: $tot"
---
+++ 2023/Day2/Day2.0.ps1
@@ -0,0 +1,67 @@
+﻿
+
+$maxRed = 12
+$maxGreen = 13
+$maxBlue = 14
+
+$red = $blue = $green = 0
+$total = $totalPowerOfCubes = 0
+
+
+foreach ($line in Get-Content '.\AoC\Day2\Day2.0.txt') {
+    Write-Host $line
+    $gameText = $line.Substring(0, $line.IndexOf(":"))
+    [string]$gameNumber = $gametext[4..$gameText.Length] -join ''
+    $gn = [int]$gameNumber
+    $fail = $false
+    $red = $blue = $green = 0
+        
+    $rounds = $($line[$($line.IndexOf(":") + 1)..$line.Length] -join '') -split ";"
+    foreach ($round in $rounds) {
+        $picks = $round -split ","
+
+        foreach ($pick in $picks) {
+            $fehs = $pick -split " "
+            $num = [int]$fehs[1] 
+            $color = $fehs[2]
+            ##write-host "num: $num color: $color"
+            switch -exact ($color) {
+                "red" {
+                    ##do red stuff here
+                    ##Write-Host "red $color $num"
+                    if ($num -gt $maxRed) { $fail = $true }
+                    if ($num -gt $red) { $red = $num }
+                    break
+                }
+                "blue" {
+                    ##do blue stuff here
+                    ##Write-Host "blue $color $num"
+                    if ($num -gt $maxBlue) { $fail = $true }
+                    if ($num -gt $blue) { $blue = $num }
+                    break
+                }
+                "green" {
+                    ##Green stuff here
+                    ##Write-Host "green $color $num"
+                    if ($num -gt $maxGreen) { $fail = $true }
+                    if ($num -gt $green) { $green = $num }
+                    break
+                }
+                Default {
+                    Write-Host "unknown color: $color"
+                    exit
+                }
+            }
+        }
+    }
+    if ($fail -eq $false) {
+        write-host "we have a winner!"
+        $total += $gn
+    }
+    $powerOfCubes = $red * $blue * $green
+    $totalPowerOfCubes += $powerOfCubes
+    Write-Host "min red: $red min blue: $blue min green: $green and thier power is: $powerOfCubes bringing the total power of cubes to: $totalPowerOfCubes"
+}
+
+Write-Host "total of game number: $total"
+Write-Host "total of power of cubes: $totalPowerOfCubes"
